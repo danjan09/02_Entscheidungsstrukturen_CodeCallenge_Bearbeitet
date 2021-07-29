ebac6211526962e2cfea7b2f4f25cfcdc88ba1d3
danjan09 <janzen.daniel@gmail.com>
2021-07-29 15:16:43 +0200

    Anfang

--- challenge_02.js
+++ challenge_02.js
@@ -7,11 +7,23 @@ Geben über eine Prompt-Anforderung ein Alter ein
 Geben Sie in Abhängigkeit vom angegebenen Alter 
 folgende Getränkeauswahl in die Konsole aus :
 
-Alter 0-5 // Milch  --> age > 0 AND age < 6
-Alter 6-12 // Saft
+Alter 0-5   // Milch  --> age > 0 AND age < 6
+Alter 6-12  // Saft
 Alter 13-17 // Cola
-18 + // Wein
+18 +        // Wein
 
---> "Max trinkt Cola."
+--> ""Name" darf "das Getränk" trinken."
+
 
 /* Getränke-Challenge */
+
+let Name;
+let Alter;
+
+Name = prompt("Bitte Name eingeben:");
+Alter = prompt("Bitte Alter eingeben:");
+
+Alter = parseInt(Alter);
+
+console.log(Name);
+console.log(Alter);
